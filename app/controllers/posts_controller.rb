ac1c85727ec1e16da7222949ed78ce188e953d93
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-07-29 19:11:55 -0400

    fix: bugfix posts_controller name; feat: develop methods in posts.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+end
